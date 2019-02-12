da6c5b568717659b11f0f5c4758de9ab1c193821
David Mason <dmason@burokuro.local>
2019-02-12 17:42:27 -0600

    + instead of =

--- EOSTools.py
+++ EOSTools.py
@@ -169,8 +169,8 @@ class EOSTools:
 
             map2command = EOSSHELL + " map link /store/user/"
             map2command = map2command + CERNname + "/ "
-            map2command + map2command + "/eos/uscms/store/user/"
-            map2command + map2command + FNALname + "/"
+            map2command = map2command + "/eos/uscms/store/user/"
+            map2command = map2command + FNALname + "/"
 
             self.logger.info("Map command 1: %s" % map1command)
             self.logger.info("Map command 2: %s" % map2command)
