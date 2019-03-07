883c14e28b9d995a696c4eb891a467f9b5f87ef7
David Mason <dmason@burokuro.local>
2019-03-07 16:51:01 -0600

    make a comment a comment

--- EmailTools.py
+++ EmailTools.py
@@ -219,7 +219,7 @@ if __name__ == '__main__':
             print ("Quietly")
 
         pwdentry=pwd.getpwnam(options.uafadd)
-        fullname=pwdentry[4]   "gecos -- user full name/comment from passwd file"
+        fullname=pwdentry[4]   #gecos -- user full name/comment from passwd file
         print ("Full name supposed to be: %s" % fullname)
         j=thingy.addToUAFList(user=options.uafadd,
                               userfullname=fullname,
